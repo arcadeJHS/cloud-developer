b5effcf677f86530486696ddb594084067f3fb0f
Matteo Piazza <matteo.piazza@exmachina.ch>
2021-01-19 21:31:07 +0100

    fix

--- course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
+++ course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
-  // apiHost: 'http://udagram-piazza-dev-dev.us-east-2.elasticbeanstalk.com/api/v0'
+  // apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagram-piazza-dev-dev.us-east-2.elasticbeanstalk.com/api/v0'
 };
 
 /*
