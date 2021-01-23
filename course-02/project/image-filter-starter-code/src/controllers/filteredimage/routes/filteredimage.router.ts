0aeab87623869e74e890453433a00cd9f921b30a
Matteo Piazza <matteo.piazza@exmachina.ch>
2021-01-23 11:50:01 +0100

    complete endpoint task

--- course-02/project/image-filter-starter-code/src/controllers/filteredimage/routes/filteredimage.router.ts
+++ course-02/project/image-filter-starter-code/src/controllers/filteredimage/routes/filteredimage.router.ts
@@ -1,46 +1,55 @@
 import { Router, Request, Response } from 'express';
-import {filterImageFromURL, deleteLocalFiles} from '../../../util/util';
+import { filterImageFromURL, deleteLocalFiles, isValidImageURL } from '../../../util/util';
 
 const router: Router = Router();
 
+/**
+ * ===> Ristrutturare meglio il codice
+ * aggiungere più typescript e tipizzazione
+ * aggiungere un file di configurazioni d'ambiente? SOLO SE SERVE (forse per accedere a S3? Ma serve? forse le imamigni sono già pubbliche)
+ */
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  // the endpoint accepts:
-  // locally:
-  // http://localhost:{{PORT}}/filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/b/bd/Golden_tabby_and_white_kitten_n01.jpg
-  // developed on AWS Beanstalk:
-  // http://{{EB_URL}}/filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/b/bd/Golden_tabby_and_white_kitten_n01.jpg
-  /**************************************************************************** */
-
-  /**
-   * ===> Ristrutturare meglio il codice
-   * come da server API: spostando da qui gli endpoint e usando i controllers
-   * aggiungere più typescript e tipizzazione
-   * aggiungere un file di configurazioni d'ambiente?
-   */
-
-  //! END @TODO1
-// ===============
+/**
+ * GET /filteredimage?image_url={{URL}}
+ * 
+ * QUERY PARAMETERS
+ * image_url: URL of a publicly accessible image
+ * 
+ * RETURNS
+ * the filtered image file
+ */
 router.get('/',
-// requireAuth,
-async (req, res) => {
-    let { image_url } = req.query;
-    return res.status(200).send(image_url);
-});
-// ===============
+    // requireAuth,
+    async (req, res) => {
+        try {
+            let { image_url } = req.query;
 
+            // Validate the image_url query
+            if (!image_url) {
+                return res.status(400).send(`image_url is required`);
+            }
+
+            // Check is an image URL
+            if (!isValidImageURL(image_url)) {
+                return res.status(400).send(`image_url not well formed`);
+            }
+
+            // Filter and image to local path
+            const filteredImageLocalPath = await filterImageFromURL(image_url);
+
+            // Send the resulting file in the response
+            await res.sendFile(filteredImageLocalPath, null, async (error) => {
+                if (error) {
+                    return res.status(500).send('Image processing error');
+                }
+                // Deletes any files on the server on finish of the response
+                await deleteLocalFiles([filteredImageLocalPath]);
+                return res.status(200);
+            });
+        }
+        catch (e) {
+            return res.status(500).send('Server error');
+        }
+    });
 
 export const FilteredimageRouter: Router = router;
